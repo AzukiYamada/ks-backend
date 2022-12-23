fb09fa4b66cf5af36fb2f6571e3073527feb5c29
Azuki Yamada <yamada@counterworks.jp>
2022-12-23 13:21:37 +0900

    showメゾットの作成

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,13 @@
 class RestaurantsController < ActionController::Base
+	def index
+		restaurants = Restaurant.all
+
+		render  json: { data: restaurants }
+	end
+
+	def show
+		restaurants = Restaurant.find(params[:id])
+
+		render json: { data: restaurants }
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants, only: %i[index]
+  get '/restaurants/:id', to: 'restaurants#show'
 end
