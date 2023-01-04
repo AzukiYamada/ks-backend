a39a156204286b2eb0559977546aab2abc27dcc9
Azuki Yamada <87220556+AzukiYamada@users.noreply.github.com>
2023-01-04 17:02:46 +0900

    インデントの修正

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,46 +1,46 @@
 class RestaurantsController < ActionController::Base
 
-	protect_from_forgery
+  protect_from_forgery
 
-	def index
-		restaurants = Restaurant.all
+  def index
+    restaurants = Restaurant.all
 
-		render  json: { data: restaurants }
-	end
+    render json: { data: restaurants }
+  end
 
-	def show
-		restaurant = Restaurant.find(params[:id])
+  def show
+    restaurant = Restaurant.find(params[:id])
 
-		render json: { data: restaurant }
-	end
+    render json: { data: restaurant }
+  end
 
-	def create
-		restaurant = Restaurant.new(restaurant_params)
-		if restaurant.save
-			render json: { data: restaurant }
-		else
-			render json: { data: restaurant.errors }
-		end
-	end
+  def create
+    restaurant = Restaurant.new(restaurant_params)
+    if restaurant.save
+      render json: { data: restaurant }
+    else
+      render json: { data: restaurant.errors }
+    end
+  end
 
-	def destroy
-		restaurant = Restaurant.find(params[:id])
-		restaurant.destroy
-		render json: { data: restaurant }
-	end
+  def destroy
+    restaurant = Restaurant.find(params[:id])
+    restaurant.destroy
+    render json: { data: restaurant }
+  end
 
-	def update
+  def update
     restaurant = Restaurant.find(params[:id])
     if restaurant.update(restaurant_params)
       render json: restaurant
     else
       render json: restaurant.errors
     end
-	end
+  end
 
-	private
+  private
 
-	def restaurant_params
+  def restaurant_params
     params.require(:restaurant).permit(:name, :description, :price, :thumbnail_url)
   end
 end
