97336da77ef1f0c2ec6c9e8fcf9f2dcaa2f3b274
Azuki Yamada <yamada@counterworks.jp>
2023-02-14 16:01:18 +0900

    updateアクションの修正

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -32,8 +32,13 @@ class RestaurantsController < ApplicationController
 
   def update
     restaurant = Restaurant.find(params[:id])
-    restaurant.update!(restaurant_params)
-    render json: { data: restaurant.as_json(include: %i[holidays reviews]) }
+    Restaurant.transaction do
+      restaurant.restaurant_holidays.each(&:destroy!)
+
+      params[:holiday_ids].each do |holiday_id|
+        restaurant.restaurant_holidays.create!(holiday_id: holiday_id)
+      end
+    end
   end
 
   private
